22151aebce26d1412fb9a88e4039e9062d25d61a
abby <bansodeabhijit1997@gmail.com>
2020-11-12 00:33:53 +0530

    added fonts and resizing for section one

--- src/components/LoginSectionOne/index.js
+++ src/components/LoginSectionOne/index.js
@@ -5,8 +5,8 @@ const LoginSectionOne = () => {
     return (
         <div className="section-1">
             <div className="logo-container" >
-                <img className="ui tiny image" src={logo} alt="Logo" />
-                <p><strong>Switch</strong>on</p>
+                <img  src={logo} alt="Logo" />
+                <p><strong>switch</strong>on</p>
             </div>
             <h3>Assignments</h3>
         </div>
