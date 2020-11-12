c2e79ecc5f047a5dfbe5f8d456c2dcf3a6e8aaa8
abby <bansodeabhijit1997@gmail.com>
2020-11-12 12:12:37 +0530

    second part login form

--- src/components/LoginPage/LoginSectionTwo/index.js
+++ src/components/LoginPage/LoginSectionTwo/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import './LoginSectionTwo.scss'
 class LoginSectionTwo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
 
+    handleChangeEmail = (event) => {
+        this.setState({ email: event.target.value });
+    }
+    handleChangePassword = (event) => {
+        this.setState({ password: event.target.value });
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert('A name was submitted: ' + this.state.email + this.state.password);
+    }
 
     render() {
         return (
             <div className="section-2">
                 <div className="login-container">
                     <h1>To-Do App</h1>
-                    
+                    <form className="ui form" onSubmit={this.handleSubmit}>
+                        <div className="ui field left icon input">
+                            <input type="email" value={this.state.email} placeholder={"Email ID"} onChange={this.handleChangeEmail} />
+                            <i className="envelope outline icon"></i>
+                        </div>
+                        <div className="ui field left icon input">
+                            <input type="text" value={this.state.password} placeholder={"Password"} onChange={this.handleChangePassword} />
+                            <i className="lock icon"></i>
+                        </div>
+                        <input className="ui field green button" type="submit" value="Submit" />
+                    </form>
                 </div>
             </div>
-
         );
     }
 
