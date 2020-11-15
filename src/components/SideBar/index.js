57000ab559b71404f069825b25825ff68eb20bbe
abby <bansodeabhijit1997@gmail.com>
2020-11-15 14:49:45 +0530

    added logout in sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../SideBar/SideBar.scss'
+import { connect } from 'react-redux';
+
+import { logout } from '../../actions';
 
 const SideBar = (props) =>{
 return(
@@ -7,11 +10,14 @@ return(
         <div className="side-bar-content">
             <div>item</div>
             <div>item</div>
-            <div>item</div>
+            <div onClick={()=>props.logout()}>item</div>
         
         </div>
     </div>
 );
 } 
 
-export default SideBar;
+const mapStateToProps = (state) => {
+    return { state: state }
+}
+export default connect(mapStateToProps, { logout })(SideBar);
