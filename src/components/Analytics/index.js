beea1b2acd34c130cb469a8a75452bb25f4e4c0f
abby <bansodeabhijit1997@gmail.com>
2020-11-16 23:33:50 +0530

    added charts.js library and implemented basic chart

--- src/components/Analytics/index.js
+++ src/components/Analytics/index.js
@@ -1,63 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './Analytics.scss'
+import {Pie, Doughnut} from 'react-chartjs-2';
 
 import ScreenHeader from '../widgets/ScreenHeader'
 
 const Analytics = (props) => {
     
+const chartData = {
+    labels: ['January', 'February', 'March',
+             'April', 'May'],
+    datasets: [
+      {
+        label: 'Rainfall',
+        backgroundColor: [
+          '#B21F00',
+          '#C9DE00',
+          '#2FDE00',
+          '#00A6B4',
+          '#6800B4'
+        ],
+        hoverBackgroundColor: [
+        '#501800',
+        '#4B5000',
+        '#175000',
+        '#003350',
+        '#35014F'
+        ],
+        data: [65, 59, 80, 81, 56]
+      }
+    ]
+  }
+  
     return (
         <div className="analytics">
             <ScreenHeader screenName="Analytics" />
-            <h3> Analytics IS SHOWN ============1</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            <h3> Analytics IS SHOWN ============</h3>
-            
+            <div>
+        <Pie
+          data={chartData}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
         </div>
     );
 }
