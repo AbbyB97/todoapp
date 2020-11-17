2f7bfbd6d47641a576e59f47c7436fd99ac57167
abby <bansodeabhijit1997@gmail.com>
2020-11-17 08:06:20 +0530

    added the legend to bottom in analytics

--- src/components/Analytics/index.js
+++ src/components/Analytics/index.js
@@ -3,71 +3,75 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import './Analytics.scss'
-import {Pie} from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 
 import ScreenHeader from '../widgets/ScreenHeader'
 
 const Analytics = (props) => {
-    console.log("====== state ",props.state.tasks);
-        
-    var toDoCount = _.countBy(props.state.tasks,  (mTask) =>{
-    
+    console.log("====== state ", props.state.tasks);
+
+    var toDoCount = _.countBy(props.state.tasks, (mTask) => {
+
         return mTask.tag === 'To-do';
 
     });
-    var inProgressCount = _.countBy(props.state.tasks,  (mTask) =>{
-    
+    var inProgressCount = _.countBy(props.state.tasks, (mTask) => {
+
         return mTask.tag === 'In-Progress';
 
     });
-    var doneCount = _.countBy(props.state.tasks,  (mTask) =>{
-    
+    var doneCount = _.countBy(props.state.tasks, (mTask) => {
+
         return mTask.tag === 'Done';
 
     });
 
-    console.log("todo count === ",toDoCount.true);
-    console.log("doneCount count === ",doneCount.true);
-    console.log("doneCount count === ",inProgressCount.true);
-const chartData = {
-    labels: ['To-do', 'In-Progress', 'Done'],
-    datasets: [
-      {
-        label: 'Rainfall',
-        backgroundColor: [
-          '#f2c94c',
-          '#27ae60',
-          '#2f80ed',
-        ],
-        hoverBackgroundColor: [
-        '#cfa936',
-        '#1c7a44',
-        '#1656ab',
-        ],
-        data: [toDoCount.true, inProgressCount.true, doneCount.true]
-      }
-    ]
-  }
-  
+    console.log("todo count === ", toDoCount.true);
+    console.log("doneCount count === ", doneCount.true);
+    console.log("doneCount count === ", inProgressCount.true);
+    const chartData = {
+        labels: ['To-do', 'In-Progress', 'Done'],
+        
+        datasets: [
+            {
+                label: 'Rainfall',
+                backgroundColor: [
+                    '#f2c94c',
+                    '#27ae60',
+                    '#2f80ed',
+                ],
+                hoverBackgroundColor: [
+                    '#cfa936',
+                    '#1c7a44',
+                    '#1656ab',
+                ],
+                data: [toDoCount.true, inProgressCount.true, doneCount.true]
+            }
+        ]
+    }
+
     return (
         <div className="analytics">
             <ScreenHeader screenName="Analytics" />
             <div>
-        <Pie
-          data={chartData}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-      </div>
+                <Pie
+                    data={chartData}
+                    options={{
+                        title: {
+                            display: true,
+                            text: 'Your task trends',
+                            fontSize: 20,
+                        },
+                        legend: {
+                            display: true,
+                            position: 'bottom',
+                            labels:{
+                            usePointStyle: true
+                            }
+                        },
+                    }}
+                />
+            </div>
         </div>
     );
 }
