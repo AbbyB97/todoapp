cd986ce9babd142c0e9762abc83abb3bcacaf526
abby <bansodeabhijit1997@gmail.com>
2020-11-17 19:46:33 +0530

    added styling according to design

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -30,26 +30,28 @@ const SideBar = (props) =>{
                 <div className="sidebar-actions">
                 <div className="tasktype-container">
                     <div>
-                <div onClick={()=>{props.showTaskType(['Personal'])}} >
+
+                    <div style={{borderRadius:"0.5rem",padding:"0.5vw",margin:"0.5vw",background:"#2f80ed",cursor:"pointer"}}  onClick={()=>{props.showTaskType(['Personal','Official','Miscellaneous'])}} >
+                     All 
+                </div>
+                <div style={{borderRadius:"0.5rem",padding:"0.5vw",margin:"0.5vw",background:"#21ba45",cursor:"pointer"}} onClick={()=>{props.showTaskType(['Personal'])}} >
                     Personal 
                 </div>
-            <div onClick={()=>{props.showTaskType(['Miscellaneous'])}} >
-                    Miscellaneous 
-                </div>
+          
                 </div>
                 <div>
-            <div onClick={()=>{props.showTaskType(['Official'])}} >
+            <div style={{borderRadius:"0.5rem",padding:"0.5vw",margin:"0.5vw",background:"#9b51e0",cursor:"pointer"}}  onClick={()=>{props.showTaskType(['Official'])}} >
                     Official 
                 </div>
-            <div onClick={()=>{props.showTaskType(['Personal','Official','Miscellaneous'])}} >
-                     All 
+                <div style={{borderRadius:"0.5rem",padding:"0.5vw",margin:"0.5vw",background:"#f2994a",cursor:"pointer"}} onClick={()=>{props.showTaskType(['Miscellaneous'])}} >
+                    Misc.
                 </div>
                 </div>
                 </div>
                 <div className="analytics-container" onClick={handleAnalytics}>analytics</div>
 
                 </div>
-            <div className="lgout-container" onClick={handleLogout}><img style={{height:"2rem",width:"2rem"}} src="https://cdn.pixabay.com/photo/2014/04/10/17/59/exit-321143_960_720.png" alt="logout"></img></div>
+            <div className="logout-container" onClick={handleLogout}><img style={{height:"2rem",width:"2rem"}} src="https://cdn.pixabay.com/photo/2014/04/10/17/59/exit-321143_960_720.png" alt="logout"></img></div>
             
         </div>
     </div>
